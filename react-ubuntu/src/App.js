11d6db0cb65212168910f2a307fdab151a19e9c3
Rocio Chavoya <rocio.chavoya@gmail.com>
2019-05-09 11:41:58 -0500

    New file components

--- react-ubuntu/src/App.js
+++ react-ubuntu/src/App.js
@@ -8,11 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h2>Welcome to Burger Queen</h2>
+        </div >
+        <button className="btn btn-primary btn-lg">ORDER</button>
+        <button className="btn btn-secondary btn-lg">COOK</button>
       </div>
     );
   }
