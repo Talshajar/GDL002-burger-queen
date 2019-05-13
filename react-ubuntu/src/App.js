2d7694dffbecaf3a1d0208d03239f20a7e440a7e
Rocio Chavoya <rocio.chavoya@gmail.com>
2019-05-13 10:09:57 -0500

    se agrega carpeta de components

--- react-ubuntu/src/App.js
+++ react-ubuntu/src/App.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import InterfaceOptions from "./Components/interface";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <div className="App-header row">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Burger Queen</h2>
-        </div >
-        <button className="btn btn-primary btn-lg">ORDER</button>
-        <button className="btn btn-secondary btn-lg">COOK</button>
-      </div>
+          <InterfaceOptions />
+     </div>
+     </div>
     );
   }
 }
---
+++ react-ubuntu/src/Components/interface.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class InterfaceOptions extends Component {
+    render () {
+        return (
+           <div >
+            <button className="btn btn-primary btn-lg">ORDER</button>
+            <button className="btn btn-secondary btn-lg">COOK</button>
+          </div>
+        )
+    }
+}
+
+export default InterfaceOptions;
