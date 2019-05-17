378d7af4a93b7ffabfef280f508d42bb68f8d2c5
Rocio Chavoya <rocio.chavoya@gmail.com>
2019-05-17 11:24:37 -0500

    se inicializa con firebase

--- react-ubuntu/src/Routes/order.js
+++ react-ubuntu/src/Routes/order.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Navigation from "./navigation";
-import ButtonBases from './viewOrder';
+import { Typography } from '@material-ui/core';
+import BreakfastItem from './src/Components/breakfast';
+import LunchItem from './src/Components/lunch';
+
+
 
 const Order = () => (
  <div>
      <Navigation />
-     <ButtonBases />
+
+     <Typography component="h2" variant="display3" gutterBottom>
+          BREAKFAST
+        </Typography>
+     <BreakfastItem/> 
+
+     <Typography component="h2" variant="display3" gutterBottom>
+          LUNCH
+        </Typography>
+     <LunchItem /> 
 
  </div>   
 );
--- react-ubuntu/src/index.js
+++ react-ubuntu/src/index.js
@@ -6,9 +6,28 @@ import Home from './Routes/home';
 import Order from './Routes/order';
 import Kitchen from './Routes/kitchen';
 import Navigation from './Routes/navigation';
-import ButtonBases from './Routes/viewOrder';
-import ViewBreak from './Routes/viewBreakfast';
-import ViewLunch from './Routes/viewLunch';
+import Firebase from 'firebase';
+
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/6.0.2/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#config-web-app -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCN9aspJu6lvfLhQCJRxA3DhES4A55xXuw",
+    authDomain: "burgerqueen-85856.firebaseapp.com",
+    databaseURL: "https://burgerqueen-85856.firebaseio.com",
+    projectId: "burgerqueen-85856",
+    storageBucket: "burgerqueen-85856.appspot.com",
+    messagingSenderId: "171137380174",
+    appId: "1:171137380174:web:cf072f9b4ce8db8f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
 
 const App = () => (
     <BrowserRouter>
@@ -16,10 +35,8 @@ const App = () => (
             <Route path="/home" component={Home} /> 
             <Route path="/kitchen" component={Kitchen} />
             <Route path="/order" component={Order} />
-            <Route path="/viewOrder" component={ButtonBases} />
             <Route path="/" component={Navigation} />
-            <Route path="/viewBreakfast" component={ViewBreak} />
-            <Route path="/viewLunch" component={ViewLunch} />
+            
       </Switch>
     </BrowserRouter>
 );
