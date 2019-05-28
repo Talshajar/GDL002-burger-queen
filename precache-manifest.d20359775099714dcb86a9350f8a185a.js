37934421dd143afa62c9330bc9a053925aeec5f0
Talshajar <rocio.chavoya@gmail.com>
2019-05-28 09:04:43 -0500

    Updates

---
+++ precache-manifest.d20359775099714dcb86a9350f8a185a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b94934383d0ce571a119f48fb4587db",
+    "url": "/rocio-burger-queen/index.html"
+  },
+  {
+    "revision": "a9d7cc909a383c884c3d",
+    "url": "/rocio-burger-queen/static/css/main.78ee6941.chunk.css"
+  },
+  {
+    "revision": "03e4240383cef1473819",
+    "url": "/rocio-burger-queen/static/js/2.53933551.chunk.js"
+  },
+  {
+    "revision": "a9d7cc909a383c884c3d",
+    "url": "/rocio-burger-queen/static/js/main.791d0556.chunk.js"
+  },
+  {
+    "revision": "b9de44e1ec1c67e2ad34",
+    "url": "/rocio-burger-queen/static/js/runtime~main.0eef7e55.js"
+  }
+]);
